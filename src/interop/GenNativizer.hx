a64a0c1b1b4bcecb19dc23c133928cdc8b3d3fc4
Kevin Leung <kevinresol@gmail.com>
2022-03-16 10:58:51 +0800

    Handle more types

--- src/interop/GenNativizer.hx
+++ src/interop/GenNativizer.hx
@@ -14,7 +14,7 @@ class GenNativizer {
 	public static function wrap(placeholder:Expr, ct:ComplexType):Function
 		return placeholder.func(['value'.toArg(ct)]);
 	public static function nullable(e:Expr):Expr
-		throw 0;
+		return macro value;
 	public static function string():Expr
 		return macro value;
 	public static function float():Expr
@@ -31,13 +31,35 @@ class GenNativizer {
 		throw 0;
 	public static function bytes():Expr
 		throw 0;
-	public static function anon(fields:Array<FieldInfo>, ct:ComplexType):Expr
-		throw 0;
-	public static function array(e:Expr):Expr
-		throw 0;
+	public static function anon(fields:Array<FieldInfo>, ct:ComplexType):Expr {
+		final ret:Array<ObjectField> = [];
+		for(f in fields) {
+			final fname = f.name;
+			ret.push({
+				field: fname,
+				expr: macro {
+					final value = value.$fname;
+					${f.expr};
+				}
+			});
+		}
+		return EObjectDecl(ret).at().log();
+	}
+	public static function array(e:Expr):Expr {
+		return
+			if(Context.defined('java'))
+				macro {
+					final list = new java.util.ArrayList();
+					for(value in value) list.add($e);
+					list;
+				}
+			else
+				macro [for(value in value) $e];
+	}
 	public static function map(k:Expr, v:Expr):Expr
 		throw 0;
 	public static function enm(constructors:Array<EnumConstructor>, ct:ComplexType, pos:Position, gen:GenType):Expr {
+		
 		return 
 			if(Context.defined('js')) {
 				final cases:Array<Case> = [];
@@ -65,19 +87,27 @@ class GenNativizer {
 					});
 				}
 				
-				ESwitch(macro value, cases, null).at(pos);
+				macro (${ESwitch(macro value, cases, null).at(pos)}:Dynamic);
 			} else {
 				macro value;
 			}
 	}
 	public static function enumAbstract(names:Array<Expr>, e:Expr, ct:ComplexType, pos:Position):Expr
-		throw 0;
+		return macro value;
 	public static function rescue(t:Type, pos:Position, gen:GenType):Option<Expr>
-		return None;
+		return Some(macro value);
 	public static function reject(t:Type):String
 		return '[interop] Unsupported type: ' + t.getID();
 	public static function shouldIncludeField(c:ClassField, owner:Option<ClassType>):Bool
-		throw 0;
-	public static function drive(type:Type, pos:Position, gen:GenType):Expr
-		return gen(type, pos);
+		return Helper.shouldIncludeField(c, owner);
+	public static function drive(type:Type, pos:Position, gen:GenType):Expr {
+		return switch type.getMeta().filter(function (m) return m.has(':interop.nativize')) {
+			case []:
+				gen(type, pos);
+			case v:
+				final m = v[0].extract(':interop.nativize');
+				final f = m[0].params[0];
+				macro ($f)(value);
+		}
+	}
 }
